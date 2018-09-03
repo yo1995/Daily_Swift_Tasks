2a17f1b70e73629f88873819035b5ac31ae7e1ce
admin <admin@students-MacBook-Air.local>
2018-09-03 12:55:30 -0400

    test console debugger lldb

--- HLWD_Swift/HLWD.playground/Contents.swift
+++ HLWD_Swift/HLWD.playground/Contents.swift
@@ -5,3 +5,11 @@ import UIKit
 var str = "Hello, playground"
 
 print(str)
+print("hello world")
+var myInt: Int?
+print(myInt as Any)
+
+var str1: String? = "test"
+if (str1 != nil) {
+    print(str1)
+}
---
+++ HLWD_Swift/Simple_Sample/Simple_Sample/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Simple_Sample
+//
+//  Created by admin on 2018/9/3.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+var myInt0: Int? = 5
+var myInt: Int?
+
+print(myInt0!)
