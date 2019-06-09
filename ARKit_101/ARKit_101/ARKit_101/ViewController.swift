efaed618b6acaeaca57284e830ae26eb3f2bf901
yo1995 <cht@bupt.edu.cn>
2019-06-08 21:13:05 -0400

    add ARKit_101 folder/project

---
+++ ARKit_101/ARKit_101/ARKit_101/ViewController.swift
@@ -0,0 +1,180 @@
+//
+//  ViewController.swift
+//  ARKit_101
+//
+//  Created by ECE564 on 6/8/19.
+//  Copyright © 2019 ECE564. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    var nodeModel:SCNNode!
+    let nodeName = "helmet_test"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        // let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
+        // let scene = SCNScene(named: "art.scnassets/helmet_test.obj")!
+        // // scene.rootNode.geometry?.firstMaterial?.blendMode = .add
+        // scene.rootNode.geometry?.firstMaterial?.diffuse.contents = nil
+        // scene.rootNode.geometry?.firstMaterial?.emission.contents = UIColor.black
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        // see the origin and coord axes
+        sceneView.debugOptions = [
+            ARSCNDebugOptions.showFeaturePoints,
+            ARSCNDebugOptions.showWorldOrigin,
+        ]
+        
+        let modelScene = SCNScene(named: "art.scnassets/helmet_test.obj")!
+        
+        nodeModel = modelScene.rootNode  // .childNode(withName: nodeName, recursively: true)
+        nodeModel.name = nodeName
+        // modelScene.rootNode.childNodes.first!.geometry!.firstMaterial!.diffuse.contents = nil
+        // 010200
+        modelScene.rootNode.childNodes.first!.geometry!.firstMaterial!.emission.contents = UIColor(valueRGB: 0x000000, alpha: 1)
+        // modelScene.rootNode.childNodes.first!.geometry!.firstMaterial!.ambientOcclusion.contents = UIColor.orange
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+        
+//        guard let lightEstimate = sceneView.session.currentFrame?.lightEstimate else { return }
+//        let intensity = lightEstimate.ambientIntensity / 1000.0
+//        // Set intensity of the lighting environment
+//        sceneView.scene.lightingEnvironment.intensity = intensity
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let location = touches.first!.location(in: sceneView)
+        
+        // Let's test if a 3D Object was touch
+        var hitTestOptions = [SCNHitTestOption: Any]()
+        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+        
+        let hitResults: [SCNHitTestResult]  = sceneView.hitTest(location, options: hitTestOptions)
+        
+        if let hit = hitResults.first {
+            if let node = getParent(hit.node) {
+                node.removeFromParentNode()
+                return
+            }
+        }
+        
+        // No object was touch? Try feature points
+        let hitResultsFeaturePoints: [ARHitTestResult]  = sceneView.hitTest(location, types: .featurePoint)
+        
+        if let hit = hitResultsFeaturePoints.first {
+            
+            // Get the rotation matrix of the camera
+            let rotate = simd_float4x4(SCNMatrix4MakeRotation(sceneView.session.currentFrame!.camera.eulerAngles.y, 0, 1, 0))
+            
+            // Combine the matrices
+            let finalTransform = simd_mul(hit.worldTransform, rotate)
+            sceneView.session.add(anchor: ARAnchor(transform: finalTransform))
+            //sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
+        }
+        
+    }
+    
+    func getParent(_ nodeFound: SCNNode?) -> SCNNode? {
+        if let node = nodeFound {
+            if node.name == nodeName {
+                return node
+            } else if let parent = node.parent {
+                return getParent(parent)
+            }
+        }
+        return nil
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async {
+                let modelClone = self.nodeModel.clone()
+                modelClone.position = SCNVector3Zero
+                
+                // Add model as a child of the node
+                node.addChildNode(modelClone)
+            }
+        }
+    }
+    
+    
+    
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
+
+
+extension UIColor {
+    convenience init(valueRGB: UInt, alpha: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat((valueRGB & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((valueRGB & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(valueRGB & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
+    }
+}
---
+++ ARKit_101/ARKit_101/README.md
@@ -0,0 +1,7 @@
+## ARKit_101
+
+有一天，想起了上学期学的 SfM 和哆啦A梦的[“模型制造相机”](https://doraemon.fandom.com/zh/wiki/%E8%BF%B7%E4%BD%A0%E9%80%9F%E5%BB%BA%E7%85%A7%E7%9B%B8%E6%A9%9F)，于是便想能不能用手上现有设备做一个类似的玩具呢？
+
+说干就干，整体流程就是拍照 - sift - Decimate - texture - ARKit
+
+目前在想这个东西还能怎么玩……
