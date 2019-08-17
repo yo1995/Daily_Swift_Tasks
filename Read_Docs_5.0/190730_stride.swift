ef9f7a10c7905cda63f9eac7752928686a42a28d
yo1995 <cht@bupt.edu.cn>
2019-08-16 23:11:03 -0400

    stride usage

---
+++ Read_Docs_5.0/190730_stride.swift
@@ -0,0 +1,7 @@
+
+let minuteInterval = 5
+let minutes = 60
+for tickMark in stride(from: 0, to: minutes, by: minuteInterval) {
+    // render the tick mark every 5 minutes (0, 5, 10, 15 ... 45, 50, 55)
+    print(tickMark)
+}
