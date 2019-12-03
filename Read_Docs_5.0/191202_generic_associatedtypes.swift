52dafde0fb7891531b7a704e15a8cd49568cb9e4
yo1995 <cht@bupt.edu.cn>
2019-12-02 21:20:02 -0500

    add generic

---
+++ Read_Docs_5.0/191202_generic_associatedtypes.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+protocol Container {
+    associatedtype Item
+    mutating func append(_ item: Item)
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+}
+
+struct Stack<Element>: Container {
+    // original Stack<Element> implementation
+    var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+    // conformance to the Container protocol
+    mutating func append(_ item: Element) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Element {
+        return items[i]
+    }
+}
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+stackOfStrings.push("cuatro")
+
+
+for _ in 0..<stackOfStrings.count {
+    print(stackOfStrings.pop())
+}
